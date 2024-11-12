7c1175afe6f6fac78feeb08018e76c38c792fc67
Redon Noah <noah.redon@student-cs.fr>
2024-11-12 15:27:00 +0100

    Random number

--- classegrille.py
+++ classegrille.py
@@ -1,7 +1,22 @@
 from random import randint
 
-def add_random(grille : dict) -> None:
-    pass #A définir
+def add_random(grille : list) -> None :
+    liste_zeros=[] #Liste qui contient les coordonnées des cases vides
+    for i in range(len(grille)) :
+        for j in range(len(grille)) :
+            if grille[i][j] == 0 :
+                liste_zeros.append((i , j))# ajoute dans liste_zeros toutes les coordonnées des cases vides
+    
+    if len(liste_zeros) > 0: #on n'effectue le programme que si la grille comporte des cases vides
+        case_select = liste_zeros[randint(0 , len(liste_zeros))] #select une case vide aléatoirement
+        (x , y) = case_select
+        proba = randint(1 , 5)
+        new_number = 0
+        if proba == 1 :
+            new_number = 4
+        else :
+            new_number = 2 #choisis le nombre à ajouter sur la case séléctionnée avec une proba de 1/5 pour 4 et 4/5 pour 2
+        grille[x][y] = new_number #modifie la grille
 
 class Grille() :
     def __init__(self , taille : int , theme : str) :
