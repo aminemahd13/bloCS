c037f1e64c155cc6e35169abaf4bce508b71c1bb
Redon Noah <noah.redon@student-cs.fr>
2024-11-14 18:11:30 +0100

    Finalisation de la classe Tuile et implémentation

--- testclassetuile/gauche.py
+++ testclassetuile/gauche.py
@@ -1,14 +1,13 @@
 from classetuile import Tuile
 
-def gauche_ligne(ligne: list) -> None:
+def gauche_ligne(ligne: list) -> bool:
     """
     Modifie la liste en fusionnant tout sur la gauche.
+    Renvoie True or False s'il y a un changement ou non.
     """
     changement = False
     # On rabbat à gauche
-    for _ in range(
-        4 - 1
-    ):  # On réalise l'opération n-1 fois pour tout bien décaler
+    for _ in range(3):  # On réalise l'opération n-1 fois pour tout bien décaler
         for j in range(1, 4):
             if ligne[j - 1].valeur == 0 and ligne[j].valeur!=0:
                 changement = True
@@ -21,9 +20,7 @@ def gauche_ligne(ligne: list) -> None:
             ligne[j - 1].valeur = ligne[j - 1].valeur * 2
             ligne[j].valeur = 0
     # On rabbat à gauche
-    for _ in range(
-        4 - 1
-    ):  # On réalise l'opération n-1 fois pour tout bien décaler
+    for _ in range(3):  # On réalise l'opération n-1 fois pour tout bien décaler
         for j in range(1, 4):
             if ligne[j - 1].valeur == 0 and ligne[j].valeur!=0:
                 changement = True
