b236ea8709b44123c14d5463e068a92a9444663d
aminemahd13 <aminemahdane13@gmail.com>
2024-11-18 01:04:17 +0100

    game

---
+++ game/settings.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame.math import Vector2 as vector
+from sys import exit
+
+
+window_wdith, window_height = 1280, 720
+
+COLORS = {
+	'white': '#f4fefa', 
+	'pure white': '#ffffff',
+	'dark': '#2b292c',
+	'light': '#c8c8c8',
+	'gray': '#3a373b',
+	'gold': '#ffd700',
+	'light-gray': '#4b484d',
+	'fire':'#f8a060',
+	'water':'#50b0d8',
+	'plant': '#64a990', 
+	'black': '#000000', 
+	'red': '#f03131',
+	'blue': '#66d7ee',
+	'normal': '#ffffff',
+	'dark white': '#f0f0f0'
+}
+
