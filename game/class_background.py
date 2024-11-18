c7197162ad7c6c5d16320c940ace7e7c618cff96
Redon Noah <noah.redon@student-cs.fr>
2024-11-18 14:38:40 +0100

    block and background 2

--- game/class_background.py
+++ game/class_background.py
@@ -1,4 +1,4 @@
-from class_block import Block, DirtBlock
+from class_block import Block, DirtBlock, StoneBlock, WoodBlock, BedrockBlock
 
 def generation_rect_to_pts(liste : list) -> list:
     """
@@ -11,82 +11,172 @@ def generation_rect_to_pts(liste : list) -> list:
                 liste_result.append([x , y])
     return liste_result
 
+
+
 class Background:
-    def __init__(self):
-        x_spawn = 0 #Indice x du block en haut à gauche lors du spawn
-        y_spawn = 0 #Indice y du block en haut à gauche lors du spawn
-        self.deplacement = 4 #Deplacement en px à chaque boucle
-        self.taille_block = 40
-        self.dict_coord = {}
-        self.dict_block = {}
+    def __init__(self , height : int , width : int):
+        self.__height = height
+        self.__width = width
+        x_spawn = 0 #Indice x en haut à gauche du bloc du spawn
+        y_spawn = 0 #Indice y en haut à gauche du bloc du spawn
+        self.__taille_block = 40
+        self.__dict_block = {}
         liste_dirt = generation_rect_to_pts([])
         liste_dirt_block = []
         for i in range(len(liste_dirt)):
             liste_dirt[i][0] = liste_dirt[i][0] - x_spawn
-            liste_dirt[i][0] = liste_dirt[i][0] * self.taille_block
+            liste_dirt[i][0] = liste_dirt[i][0] * self.__taille_block
             liste_dirt[i][1] = liste_dirt[i][1] - y_spawn
-            liste_dirt[i][1] = liste_dirt[i][1] * self.taille_block
+            liste_dirt[i][1] = liste_dirt[i][1] * self.__taille_block
             liste_dirt_block.append(DirtBlock(x = liste_dirt[i][0] , y = liste_dirt[i][1]))
-        self.dict_coord["Dirt"] = liste_dirt
-        self.dict_block["Dirt"] = liste_dirt_block
-        
-        
-    def right(self) -> None:
+        self.__dict_block["Dirt"] = liste_dirt_block
+
+    
+     
+    def right(self , deplacement : int) -> None:
         """
         Déplace tout les blocs sur la gauche.
         """
-        for liste in self.dict_block.values():
+        for liste in self.__dict_block.values():
             for block in liste:
-                block.x = block.x - self.deplacement
-        for liste in self.dict_coord.values():
-            for block in liste:
-                block[0] = block[0] - self.deplacement
+                block.x = block.x - deplacement
     
-    def left(self) -> None:
+    
+    def left(self , deplacement : int) -> None:
         """
         Déplace tout les blocs sur la droite.
         """
-        for liste in self.dict_block.values():
+        for liste in self.__dict_block.values():
             for block in liste:
-                block.x = block.x + self.deplacement
-        for liste in self.dict_coord.values():
-            for block in liste:
-                block[0] = block[0] + self.deplacement
+                block.x = block.x + deplacement
     
-    def up(self) -> None:
+    
+    def up(self , deplacement : int) -> None:
         """
         Déplace tout les blocs sur le bas.
         """
-        for liste in self.dict_block.values():
+        for liste in self.__dict_block.values():
             for block in liste:
-                block.y = block.y + self.deplacement
-        for liste in self.dict_coord.values():
-            for block in liste:
-                block[1] = block[1] + self.deplacement
+                block.y = block.y + deplacement
     
-    def down(self) -> None:
+    
+    def down(self , deplacement : int) -> None:
         """
         Déplace tout les blocs sur le haut.
         """
-        for liste in self.dict_block.values():
+        for liste in self.__dict_block.values():
             for block in liste:
-                block.y = block.y - self.deplacement
-        for liste in self.dict_coord.values():
-            for block in liste:
-                block[1] = block[1] - self.deplacement
+                block.y = block.y - deplacement
     
-    def check_block(self , x : int , y : int) -> str:
+    
+    def check_block(self , x : int , y : int) -> Block:
         """
-        Renvoie le type du block en x , y.
+        Renvoie le bloc en x , y.
         """
-        for cle , liste in self.dict_coord.items():
+        for liste in self.__dict_block.values():
             for block in liste:
-                if block[0] <= x < block[0] + self.taille_block and block[1] <= y < block[0] + self.taille_block:
-                    return cle
+                if block.x <= x < block.x + self.__taille_block and block.y <= y < block.y + self.__taille_block:
+                    return block
         return None
     
-    def add_block(self , block : Block) -> None:
+    
+    def add_block(self , block : Block , decalage_auto : bool = True) -> bool:
         """
-        Ajoute un block dans le background
+        Ajoute un bloc dans le background.
+        Renvoie True si le bloc a été placé, False sinon.
         """
-        
+        if self.check_block(x = block.x , y = block.y) is None:
+            if decalage_auto:
+                x = self.__dict_block["Dirt"][0].x
+                y = self.__dict_block["Dirt"][0].y
+                while x < block.x:
+                    x += self.__taille_block
+                while x > block.x:
+                    x -= self.__taille_block
+                while y < block.y:
+                    y += self.__taille_block
+                while y > block.y:
+                    y -= self.__taille_block
+                block.x = x
+                block.y = y
+            self.__dict_block[block.type].append(block)
+            return True
+        return False
+    
+    
+    def damage_block(self , x : int , y : int , damage : int) -> bool:
+        """
+        Attaque le bloc se situant en x , y.
+        S'il y avait un bloc qui a été détruit, renvoie True.
+        Sinon, renvoie False.
+        """
+        for liste in self.__dict_block.values():
+            for i in range(len(liste)):
+                if liste[i].x <= x < liste[i].x + self.__taille_block and liste[i].y <= y < liste[i].y + self.__taille_block:
+                    if liste[i].take_damage(damage = damage):
+                        liste.pop(i)
+                        return True
+                    return False
+        return False
+    
+    
+    def render(self) -> None:
+        for liste in self.__dict_block.values():
+            for block in liste:
+                if 1 - self.__taille_block <= block.x <= self.__width and 1 - self.__taille_block <= block.y <= self.__height:
+                    block.render()
+
+"""
+Utilisation
+
+
+
+Initialisation du background :
+background = Background(width : int , height : int)
+
+
+
+Déplacer le joueur à droite (<=> déplacer le background à gauche) de deplacement pixels :
+background.right(deplacement : int) -> None
+
+
+
+Déplacer le joueur à gauche (<=> déplacer le background à droite) de deplacement pixels :
+background.left(deplacement : int) -> None
+
+
+
+Déplacer le joueur en haut (<=> déplacer le background en bas) de deplacement pixels :
+background.up(deplacement : int) -> None
+
+
+
+Déplacer le joueur à bas (<=> déplacer le background en haut) de deplacement pixels :
+background.down(deplacement : int) -> None
+
+
+
+background.add_block(block : Block , decalage_auto : bool = True) -> bool
+Ajoute un bloc en x,y.
+Renvoie True si le bloc a été placé (s'il y avait pas de bloc ici), False sinon.
+
+Remarque : Imaginons qu'un bloc peut être placé en coordonnées haut-gauche (0,0) pour avoir le bon décalage
+avec le reste de la map. Si on veut ajouter le bloc en (1,1), ça va automatiquement le mettre
+en (0,0) pour avoir le bon décalage avec le reste de la map.
+Cette option peut être désactivée.
+
+
+
+background.damage_block(x : int , y : int , damage : int) -> bool
+Attaque le bloc, s'il existe, en x,y.
+Renvoie True s'il y avait un bloc qui a été détruit, False sinon.
+
+
+
+background.check_block(x : int , y : int) -> Block
+Renvoie le bloc se situant en x,y.
+Renvoie None s'il n'y a pas de block.
+
+
+
+"""
--- game/class_block.py
+++ game/class_block.py
@@ -5,39 +5,33 @@ class Block:
     def __init__(self, x : int , y : int , is_solid : bool = True, breakable : bool = True , health : int = 100):
         """
         Initialize a block.
-        :param position: A tuple (x, y) representing the block's position.
+        :params x,y : Representing the block's position.
         :param is_solid: Whether the block can be passed through.
         :param breakable: Whether the block can be broken.
         :param health: The starting health of the block.
         """
         self.x = x
         self.y = y
+        self.type = None
         self.is_solid = is_solid
         self.breakable = breakable
         self.health = health
 
-    def take_damage(self, amount : int) -> None:
+    def take_damage(self , damage : int) -> bool:
         """
-        Reduces the block's health.
-        :param amount: The amount of damage to apply.
+        Réduit la vie du bloc.
+        Renvoie True si le bloc est cassé, False sinon.
         """
         if self.breakable:
-            self.health -= amount
+            self.health -= damage
             if self.health <= 0:
-                self.break_block()
-        else:
-            print(f"This block is unbreakable!")
-
-    def break_block(self):
-        """
-        Handles block destruction.
-        """
-        print(f"The block at {(self.x , self.y)} is destroyed!")
-        # Remove block logic here
+                return True
+            return False
+        return False
 
     def render(self):
         """
-        Renders the block.
+        Affiche le bloc.
         """
         print(f"Rendering block at {(self.x , self.y)}")
 
@@ -45,34 +39,44 @@ class Block:
 
 
 class DirtBlock(Block):
-    def __init__(self, position):
-        super().__init__(position, is_solid=True, breakable=True, health=50)
-
-    def render(self):
-        print(f"Rendering block at {self.position}")
-
+    def __init__(self , x : int , y : int):
+        super().__init__(x = x , y = y , is_solid = True , breakable = True , health = 50)
+        self.type = "Dirt"
 
 
 class StoneBlock(Block):
-    def __init__(self, position):
-        super().__init__(position, is_solid=True, breakable=True, health=200)
-
-    def render(self):
-        print(f"Rendering block at {self.position}")
-
+    def __init__(self , x : int , y : int):
+        super().__init__(x = x , y = y , is_solid = True , breakable = True , health = 200)
+        self.type = "Stone"
 
 
 class WoodBlock(Block):
-    def __init__(self, position):
-        super().__init__(position, is_solid=True, breakable=True, health=100)
-
-    def render(self):
-        print(f"Rendering block at {self.position}")
+    def __init__(self , x : int , y : int):
+        super().__init__(x = x , y = y , is_solid = True , breakable = True , health = 100)
+        self.type = "Wood"
 
 
-class BedRock(Block):
-    def __init__(self, position):
-        super().__init__(position, is_solid=True, breakable=False, health=100)
+class BedrockBlock(Block):
+    def __init__(self , x : int , y : int):
+        super().__init__(x = x , y = y , is_solid = True , breakable = False , health = 100)
+        self.type = "BedRock"
 
-    def render(self):
-        print(f"Rendering block at {self.position}")
+
+"""
+Utilisation
+
+Initialisation :
+wood = WoodBlock(x : int , y : int)
+stone = StoneBlock(x : int , y : int)
+dirt = DirtBlock(x : int , y : int)
+bedrock = BedrockBlock(x : int , y : int)
+
+Attaquer un bloc :
+block.take_damage(damage : int) -> bool
+Attaque le bloc. Renvoie True si le bloc est détruit, False sinon.
+
+Afficher le bloc :
+block.render() -> None
+Affiche le bloc
+
+"""
