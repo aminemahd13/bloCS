e7dde3725f1fb892124dcfcee3e722e42a316664
Redon Noah <noah.redon@student-cs.fr>
2024-11-18 15:02:20 +0100

    Commentaires class background

--- game/class_background.py
+++ game/class_background.py
@@ -15,13 +15,14 @@ def generation_rect_to_pts(liste : list) -> list:
 
 class Background:
     def __init__(self , height : int , width : int):
-        self.__height = height
-        self.__width = width
+        self.__height = height #Hauteur de l'écran
+        self.__width = width #Largeur de l'écran
         x_spawn = 33 #Indice x en haut à gauche du bloc du spawn
         y_spawn = 6 #Indice y en haut à gauche du bloc du spawn
         self.__taille_block = 40
-        self.__dict_block = {}
+        self.__dict_block = {} #Dictionnaire contenant tout les blocs
         
+        #On ajoute tout les blocs de type Dirt
         liste_dirt = generation_rect_to_pts([])
         liste_dirt_block = []
         for i in range(len(liste_dirt)):
@@ -34,6 +35,7 @@ class Background:
             liste_dirt_block.append(DirtBlock(x = liste_dirt[i][0] , y = liste_dirt[i][1]))
         self.__dict_block["Dirt"] = liste_dirt_block
 
+        #On ajoute tout les blocs de type Stone
         liste_stone = generation_rect_to_pts([])
         liste_stone_block = []
         for i in range(len(liste_stone)):
@@ -46,6 +48,7 @@ class Background:
             liste_stone_block.append(StoneBlock(x = liste_stone[i][0] , y = liste_stone[i][1]))
         self.__dict_block["Stone"] = liste_stone_block
         
+        #On ajoute tout les blocs de type Wood
         liste_wood = generation_rect_to_pts([])
         liste_wood_block = []
         for i in range(len(liste_wood)):
@@ -57,7 +60,8 @@ class Background:
             liste_wood[i][1] = liste_wood[i][1] + self.__height // 2
             liste_wood_block.append(WoodBlock(x = liste_wood[i][0] , y = liste_wood[i][1]))
         self.__dict_block["Wood"] = liste_wood_block
-    
+
+        #On ajoute tout les blocs de type Bedrock
         liste_bedrock = generation_rect_to_pts([])
         liste_bedrock_block = []
         for i in range(len(liste_bedrock)):
@@ -114,6 +118,7 @@ class Background:
         for liste in self.__dict_block.values():
             for block in liste:
                 if block.x <= x < block.x + self.__taille_block and block.y <= y < block.y + self.__taille_block:
+                    #On regarde si x,y est compris dans les coordonnées du bloc avec sa taille
                     return block
         return None
     
@@ -124,7 +129,7 @@ class Background:
         Renvoie True si le bloc a été placé, False sinon.
         """
         if self.check_block(x = block.x , y = block.y) is None:
-            if decalage_auto:
+            if decalage_auto: #Ajustement automatique
                 x = self.__dict_block["Dirt"][0].x
                 y = self.__dict_block["Dirt"][0].y
                 while x < block.x:
@@ -151,6 +156,7 @@ class Background:
         for liste in self.__dict_block.values():
             for i in range(len(liste)):
                 if liste[i].x <= x < liste[i].x + self.__taille_block and liste[i].y <= y < liste[i].y + self.__taille_block:
+                    #On regarde si x,y est compris dans les coordonnées du bloc avec sa taille
                     if liste[i].take_damage(damage = damage):
                         liste.pop(i)
                         return True
@@ -162,6 +168,7 @@ class Background:
         for liste in self.__dict_block.values():
             for block in liste:
                 if 1 - self.__taille_block <= block.x <= self.__width and 1 - self.__taille_block <= block.y <= self.__height:
+                    #On affiche uniquement les blocs qui se situent dans la map
                     block.render()
 
 """
