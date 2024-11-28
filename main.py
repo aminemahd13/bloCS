31da1e349f8e78d262dd57fde79be91df087c107
Redon Noah <noah.redon@student-cs.fr>
2024-11-28 17:32:44 +0100

    test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from classes.class_background import Background
 from classes.class_entities import Entities
+import utils.key_handler as key
 
 player_name = "Player 1"
 height_screen = 1080
@@ -19,10 +20,30 @@ background = Background()
 
 #Create the player
 running = entities.initialize(player_name = player_name) #Ecran d'accueil. Renvoie True si l'utilisateur veut jouer
+
+#Si on run, on envoie une demande de jeu au serveur
+#Si elle est acceptée, alors on joue
+
 # Game loop
 clock = pygame.time.Clock()
 
 while running:
+    data_sent = {
+        "right" : key.right(),
+        "left" : key.left(),
+        "up" : key.up(),
+        "echap" : key.close(),
+        "number" : key.get_number(),
+        "click" : None
+    }
+    #On regare où il click
+    
+    
+    data_received = {
+        "Player" : 
+    }
+    
+    
     #Envoyer les données du clavier et de la souris
     #Recevoir les données et actualiser background et entities
     entities.render(player_name = player_name , background = background)
